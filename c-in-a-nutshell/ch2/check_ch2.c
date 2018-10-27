e6c3931386e7d733853e1d5a45ef05d4102ec0d1
banxi1988 <banxi1988@gmail.com>
2018-10-27 18:01:09 +0800

    fix printf format warning

--- c-in-a-nutshell/ch2/check_ch2.c
+++ c-in-a-nutshell/ch2/check_ch2.c
@@ -10,10 +10,10 @@ int main(int argc, char const *argv[]) {
   printf("输出类型信息\n");
   printf("默认的 char 类型：%s\n",CHAR_MIN < 0 ? "signed":"unsigned");
   printf("%10s\t%10s\t%4s\t%20s\t%20s\n","类型","字节数","位数","最小值","最大值");
-  printf("%10s\t%10zu\t%4zu\t%20d\t%20d\n","char",sizeof(char),CHAR_BIT, CHAR_MIN,CHAR_MAX);
-  printf("%10s\t%10zu\t%4zu\t%20d\t%20d\n","short",sizeof(short),0, SHRT_MIN,SHRT_MAX);
-  printf("%10s\t%10zu\t%4zu\t%20d\t%20d\n","int",sizeof(int),0, INT_MIN,INT_MAX);
-  printf("%10s\t%10zu\t%4zu\t%20ld\t%20ld\n","long",sizeof(long),LONG_BIT, LONG_MIN,LONG_MAX);
-  printf("%10s\t%10zu\t%4zu\t%20ld\t%20ld\n","long long",sizeof(long long),0, LLONG_MIN,LLONG_MAX);
+  printf("%10s\t%10zu\t%4d\t%20d\t%20d\n","char",sizeof(char),CHAR_BIT, CHAR_MIN,CHAR_MAX);
+  printf("%10s\t%10zu\t%4d\t%20d\t%20d\n","short",sizeof(short),0, SHRT_MIN,SHRT_MAX);
+  printf("%10s\t%10zu\t%4d\t%20d\t%20d\n","int",sizeof(int),0, INT_MIN,INT_MAX);
+  printf("%10s\t%10zu\t%4d\t%20ld\t%20ld\n","long",sizeof(long),LONG_BIT, LONG_MIN,LONG_MAX);
+  printf("%10s\t%10zu\t%4d\t%20lld\t%20lld\n","long long",sizeof(long long),0, LLONG_MIN,LLONG_MAX);
   return 0;
 }
