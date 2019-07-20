50deb7edc30ef7531e337e4997585e6ee99fe7ee
banxi1988 <banxi1988@gmail.com>
2019-07-21 07:28:38 +0800

    add(csapp): 添加过程调用相关备注

---
+++ csapp/asm/area_price.c
@@ -0,0 +1,16 @@
+
+long calc_area(long width, long height) {
+  return width * height;
+}
+long calc_area_price(long width, long height, long unit_price) {
+  long area = calc_area(width, height);
+  long price = area * unit_price;
+  return price;
+}
+long main() {
+  long width = 3;
+  long height = 4;
+  long unit_price = 5;
+  long price = calc_area_price(width, height, 5);
+  return price;
+}
